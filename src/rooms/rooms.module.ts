e89113298aff4544d107587126d3f7a80304284b
Andrew Horn <andyjhorn@gmail.com>
2022-07-03 22:45:09 -0500

    removed rooms controller

--- src/rooms/rooms.module.ts
+++ src/rooms/rooms.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { RoomsService } from './rooms.service';
-import { RoomsController } from './rooms.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Room } from './room.entity';
 
@@ -9,7 +8,6 @@ import { Room } from './room.entity';
     TypeOrmModule.forFeature([Room])
   ],
   providers: [RoomsService],
-  controllers: [RoomsController],
   exports: [RoomsService]
 })
 export class RoomsModule { }
